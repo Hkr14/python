abcbb49abf62177f602c354c21abba9a60d4c031
Hkr14 <cesar14111907@gmail.com>
2023-10-28 18:39:35 -0700

    update plugins/gates/rp.py

--- plugins/gates/rp.py
+++ plugins/gates/rp.py
@@ -21,7 +21,7 @@ from datos import idchat
 @Client.on_message(filters.command(["rp"], ["/", "."]))
 async def cr(_, message: Message):
     
-    with open(file='plugins/usuarios/premium.txt',mode='r+',encoding='utf-8') as archivo:
+    with open('plugins/usuarios/premium.txt',mode='r+',encoding='utf-8') as archivo:
       x = archivo.readlines()
       if str(message.from_user.id) + '\n' in x or message.chat.id in idchat:
 
